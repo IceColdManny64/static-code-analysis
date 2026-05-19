6542c299237e99bd5cdf2dcb6aa880a11a6e31d1
JOSÉ MANUEL JUÁREZ DÍAZ <22151192@aguascalientes.tecnm.mx>
2026-05-18 22:57:07 -0600

    first commit

---
+++ tests/Unit/OperationsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class OperationsController extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function addition(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+}
