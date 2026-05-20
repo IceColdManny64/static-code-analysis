f0c25d7ccfd2210bfd70b4d96fcbc0a24f348bfc
JOSÉ MANUEL JUÁREZ DÍAZ <22151192@aguascalientes.tecnm.mx>
2026-05-19 21:50:35 -0600

    pruebas workflow

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,16 +4,21 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\Unit\OperationsController;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_addition_result(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $controller = new OperationsController;
+        
+        $result = $controller->addition(4, 9);
+        $this->assertEquals(13, $result);
+        $this->assertIsInt($result);
+        $this->assertNotNull($result);
+        $this->assertGreaterThan(0, $result);
     }
 }
